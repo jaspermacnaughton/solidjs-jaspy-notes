592c08c7afbca9408426f05d3425bda8695d5e61
Jasper MacNaughton <jaspermacnaughton@gmail.com>
2024-11-07 18:42:10 -0600

    Remove id from notecard from debugging

--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -12,7 +12,7 @@ const NoteCard: Component<NoteCardProps> = (props) => {
     <div class="bg-white pt-2 p-4 ml-0 mb-0 m-4 text-center rounded-md shadow-md">
       <div class="flex items-center justify-between w-full">
         <div class="w-6"></div>
-        <h2 class="flex-grow"><b>{props.title}</b> ({props.id})</h2>
+        <h2 class="flex-grow"><b>{props.title}</b></h2>
         <span class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
           onClick={() => props.onDelete(props.id)}>
           delete
