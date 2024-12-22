d80d9a6a17e2ddae2e36dd78ed770df8a2032db0
Jasper MacNaughton <jaspermacnaughton@gmail.com>
2024-12-21 18:40:56 -0800

    Trigger textarea on clicking edit

--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -1,4 +1,4 @@
-import type { Component } from 'solid-js';
+import { createSignal, type Component } from 'solid-js';
 
 type NoteCardProps = {
   note_id: number;
@@ -9,6 +9,8 @@ type NoteCardProps = {
 }
 
 const NoteCard: Component<NoteCardProps> = (props) => {
+  const [isEditing, setIsEditing] = createSignal(false);
+
   return (
     <div class="bg-white p-2 ml-0 mb-0 m-4 text-center rounded-md shadow-md">
       <div class="flex items-center justify-between w-full mb-1">
@@ -22,14 +24,30 @@ const NoteCard: Component<NoteCardProps> = (props) => {
       
       <hr class="mb-2" />
       
-      <p>{props.body}</p>
-      
-      <div class="flex items-center justify-end w-full mt-2">
-        <span class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
-          onClick={() => props.onEdit(props.note_id)}>
-          edit
-        </span>
-      </div>
+      {isEditing() ? (
+        <>
+          <textarea value={props.body} class="w-full bg-gray-50 border border-gray-300 rounded-md p-1" />
+          <div class="flex items-center justify-end w-full mt-2">
+            <span class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
+              onClick={() => {
+                props.onEdit(props.note_id);
+                setIsEditing(false);
+              }}>
+              save
+            </span>
+          </div>
+        </>
+      ) : (
+        <>
+          <p>{props.body}</p>
+          <div class="flex items-center justify-end w-full mt-2">
+            <span class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
+              onClick={() => setIsEditing(true)}>
+              edit
+            </span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
