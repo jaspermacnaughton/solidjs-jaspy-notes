608c32665a728822d71663c63d89d0ae5de9ea39
Jasper MacNaughton <jaspermacnaughton@gmail.com>
2024-12-31 02:33:35 -0900

    Move subitems down (and out of isEditing body)

--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -66,97 +66,78 @@ const NoteCard: Component<NoteCardProps> = (props) => {
       
       <hr class="mb-2" />
       
-      {isEditing() ? (
-        <div class="flex flex-col flex-grow">{/* Editing note display*/}
-          <textarea 
-            id={`note-${props.note_id}-body`}
-            class="w-full h-full bg-gray-50 border border-gray-300 rounded-md p-1 resize-none" 
-            value={currentBody()}
-            onInput={(e) => setCurrentBody(e.currentTarget.value)}
-            rows={currentBody().split('\n').length}
-          />
-          
-          <div class="flex flex-col gap-2 mt-2">
-            <For each={currentSubitems()}>
-              {(subitem) => (
-                <div class="flex items-center gap-2 p-1 border border-gray-200 rounded-md">
-                  <input 
-                    id={`subitem-${subitem.subitem_id}-checkbox`}
-                    type="checkbox" 
-                    class="w-4 h-4 m-2 mr-1 accent-emerald-600 cursor-pointer" 
-                    checked={subitem.is_checked}
-                    onChange={() => toggleSubitem(subitem)}
-                  />
-                  <textarea 
-                    id={`subitem-${subitem.subitem_id}-text`}
-                    class={`flex-grow whitespace-pre-wrap text-left bg-gray-50 border border-gray-300 rounded-md p-1 resize-none ${
-                      subitem.is_checked ? 'line-through text-gray-500' : ''
-                    }`}
-                    value={subitem.text}
-                    rows={subitem.text.split('\n').length}
-                    onfocusout={(e) => updateSubitemText(subitem, e.currentTarget.value)}
-                  />
-                  <span class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
-                    onClick={() => deleteSubitem(subitem)}
-                  >
-                    delete
-                  </span>
-                </div>
-              )}
-            </For>
-          </div>
-          
-          <div class="flex items-center justify-between w-full mt-2">
-            <button class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
-              onClick={() => {
-                setCurrentBody(props.body);
-                setIsEditing(false);
-              }}>
-              cancel
-            </button>
-            <button class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
-              onClick={addSubitem}>
-              add_circle
-            </button>
-            <button class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
-              onClick={handleSave}>
-              save
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div class="flex flex-col flex-grow">{/* Viewing note display*/}
-          <p class="flex-grow whitespace-pre-wrap text-left border border-transparent rounded-md p-1">{props.body}</p>
-          
-          <div class="flex flex-col gap-2 mt-2">
-            <For each={currentSubitems()}>
-              {(subitem) => (
-                <div class="flex items-center gap-2 p-1 border border-gray-200 rounded-md">
-                  <input 
-                    id={`subitem-${subitem.subitem_id}-checkbox`}
-                    type="checkbox" 
-                    class="w-4 h-4 m-2 mr-1 accent-emerald-600 cursor-pointer" 
-                    checked={subitem.is_checked}
-                    onChange={() => toggleSubitem(subitem)}
-                  />
-                  <p class={`flex-grow whitespace-pre-wrap text-left border border-transparent rounded-md p-1 ${
+      <div class="flex flex-col flex-grow">
+        {isEditing() ? (
+          <>{/* Editing note display*/}
+            <textarea 
+              id={`note-${props.note_id}-body`}
+              class="w-full h-full bg-gray-50 border border-gray-300 rounded-md p-1 resize-none" 
+              value={currentBody()}
+              onInput={(e) => setCurrentBody(e.currentTarget.value)}
+              rows={currentBody().split('\n').length}
+            />
+            
+            <div class="flex items-center justify-between w-full mt-2">
+              <button class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
+                onClick={() => {
+                  setCurrentBody(props.body);
+                  setIsEditing(false);
+                }}>
+                cancel
+              </button>
+              <button class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
+                onClick={addSubitem}>
+                add_circle
+              </button>
+              <button class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
+                onClick={handleSave}>
+                save
+              </button>
+            </div>
+          </>
+        ) : (
+          <>{/* Viewing note display*/}
+            <p class="flex-grow whitespace-pre-wrap text-left border border-transparent rounded-md p-1">{props.body}</p>
+            
+            <div class="flex items-center justify-end w-full mt-2">
+              <button class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
+                onClick={() => setIsEditing(true)}>
+                edit
+              </button>
+            </div>
+          </>
+        )}
+      
+        <div class="flex flex-col gap-2 mt-2">
+          <For each={currentSubitems()}>
+            {(subitem) => (
+              <div class="flex items-center gap-2 p-1 border border-gray-200 rounded-md">
+                <input 
+                  id={`subitem-${subitem.subitem_id}-checkbox`}
+                  type="checkbox" 
+                  class="w-4 h-4 m-2 mr-1 accent-emerald-600 cursor-pointer" 
+                  checked={subitem.is_checked}
+                  onChange={() => toggleSubitem(subitem)}
+                />
+                <textarea 
+                  id={`subitem-${subitem.subitem_id}-text`}
+                  class={`flex-grow whitespace-pre-wrap text-left bg-gray-50 border border-gray-300 rounded-md p-1 resize-none ${
                     subitem.is_checked ? 'line-through text-gray-500' : ''
-                  }`}>
-                    {subitem.text}
-                  </p>
-                </div>
-              )}
-            </For>
-          </div>
-          
-          <div class="flex items-center justify-end w-full mt-2">
-            <button class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
-              onClick={() => setIsEditing(true)}>
-              edit
-            </button>
-          </div>
+                  }`}
+                  value={subitem.text}
+                  rows={subitem.text.split('\n').length}
+                  onfocusout={(e) => updateSubitemText(subitem, e.currentTarget.value)}
+                />
+                <span class="w-6 material-symbols-outlined hover:bg-neutral-800 hover:text-white cursor-pointer rounded-sm align-middle"
+                  onClick={() => deleteSubitem(subitem)}
+                >
+                  delete
+                </span>
+              </div>
+            )}
+          </For>
         </div>
-      )}
+      </div>
     </div>
   );
 };
